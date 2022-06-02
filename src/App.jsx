d4caf24d4830aae2fc2a5c63db46bc167852f857
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-06-02 13:20:33 -0300

    criação do componente Sidebar e estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Samuel Menecucci"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum iure voluptatem assumenda? Ex assumenda suscipit possimus voluptas, laborum ipsum, alias totam repellendus eaque unde officiis aliquid placeat explicabo quod inventore."
-      />
-      <Post content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum iure voluptatem assumenda? Ex assumenda suscipit possimus voluptas, laborum ipsum, alias totam repellendus eaque unde officiis aliquid placeat explicabo quod inventore." />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Samuel Menecucci"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum iure voluptatem assumenda? Ex assumenda suscipit possimus voluptas, laborum ipsum, alias totam repellendus eaque unde officiis aliquid placeat explicabo quod inventore."
+          />
+          <Post content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum iure voluptatem assumenda? Ex assumenda suscipit possimus voluptas, laborum ipsum, alias totam repellendus eaque unde officiis aliquid placeat explicabo quod inventore." />
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import { PencilLine } from "phosphor-react"; //lib de ícones utilizadas na aplicação
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/samuelmenecucci.png"
+          alt=""
+        />
+        <strong>Samuel Menecucci</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
